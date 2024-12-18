3f0f1a473006fa8babe11282f669d6f779d59dde
RajeenKhadka <rajeenkhadka315@gmail.com>
2024-12-17 22:13:28 -0500

    Add & Delete is functioning

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ToDoList from "./pages/ToDoList";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Create Todo List</h1>
+        <ToDoList />
+      </div>
+    </>
+  );
+}
+
+export default App;
